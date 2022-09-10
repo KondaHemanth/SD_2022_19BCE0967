bd0afdbe4bd68f07f9019328c372ae953342be6a
KondaHemanth <pavanshemanth@gmail.com>
2022-09-10 19:01:05 +0530

    Pawn movements

--- Chess.java
+++ Chess.java
@@ -48,6 +48,7 @@ class Chess {
     }
 
     public static void printBoard() {
+        
         for(int i=0; i<5; i++){
             for(int j=0; j<5; j++){
                 System.out.print(board[i][j]+ "\t");
@@ -64,23 +65,127 @@ class Chess {
         inp = scn.nextLine();
 
         String character = inp.substring(0, inp.indexOf(':')).trim();
-        int pos = Integer.valueOf(inp.substring(inp.indexOf(':')+1).trim());
-        updateBoard(player, character, pos);
-    }
-    
-    
-    public static void updateBoard(char player, String character, int position) {
-        int row = position/5, col=position%5;
+
+        char move = inp.charAt(inp.indexOf(':')+1);
+        int movCode;
+
+        switch(move){
+            case 'F':
+                movCode = 1;
+                break;
+            case 'B':
+                movCode = -1;
+                break;
+            case 'R':
+                movCode = 2;
+                break;
+            default:
+                movCode = -2;
+        }
+
+        //int finPos = Integer.valueOf(inp.substring(inp.indexOf(':')+1).trim());
+        int pos;
+        //updateBoard(player, character, pos);
 
         if(player == 'A'){
-            A.put(character, position);
+            
+            updateBoard('A', character, movCode);
         }
         else {
-            B.put(character, position);
+            
+            updateBoard('B', character, movCode);
+        }
+    }
+    
+
+    
+    public static void updateBoard() {
+        int pos;
+        
+        for (Map.Entry<String,Integer> ele : A.entrySet()) {
+            pos = ele.getValue();
+            
+            board[(pos/5)][(pos%5)] = "A-" + ele.getKey();
         }
         
-        // To bo changed to implement eating
-        board[row][col] = player + "-" +character;
+        for (Map.Entry<String,Integer> ele : B.entrySet()) {
+            pos = ele.getValue();
+            
+            board[pos/5][pos%5] = "B-" + ele.getKey();
+        }
+    }
+    
+    public static void updateBoard(char player, String character, int code) {
+        int pos, finPos=0;
+        
+        if(player == 'A'){
+
+            pos = A.get(character);
+
+            if(code == 1 || code == -1){
+                if(code == 1){
+                    finPos = pos - 5;
+                }
+                else {
+                    finPos = pos + 5;
+                }
+                
+            }
+            else {
+                if(code == -2){
+                    finPos = pos - 1;
+                }
+                else {
+                    finPos = pos + 1;
+                }
+            }
+
+            if(finPos/5 > 4 || finPos/5 < 0 || finPos%5 > 4 || finPos % 5 < 0){
+
+                System.out.println("Move out of bounds");
+
+                return;
+            }
+
+            board[pos/5][pos%5] = "-";
+            
+            A.put(character, finPos);
+            board[finPos/5][finPos%5] = "A-" + character ;
+            
+        }
+        else {
+            pos = B.get(character);
+
+            if(code == 1 || code == -1){
+                if(code == 1){
+                    finPos = pos + 5;
+                }
+                else {
+                    finPos = pos - 5;
+                }
+                
+            }
+            else {
+                if(code == -2){
+                    finPos = pos - 1;
+                }
+                else {
+                    finPos = pos + 1;
+                }
+            }
+
+            if(finPos/5 > 4 || finPos/5 < 0 || finPos%5 > 4 || finPos % 5 < 0){
+
+                System.out.println("Move out of bounds");
+
+                return;
+            }
+
+            board[pos/5][pos%5] = "-";
+            
+            B.put(character, finPos);
+            board[finPos/5][finPos%5] = "B-" + character ;
+        }
     }
     
     public static void getInput(char player, String cmd) {
@@ -94,11 +199,15 @@ class Chess {
 
         if(player == 'A'){
             for(int i=0; i<chars.length; i++)
-               updateBoard('A', chars[i].trim(), i+20);
+                A.put(chars[i].trim(), i+20);                
+                //updateBoard('A', chars[i].trim(), i+20);
         }
         else if(player == 'B'){
             for(int i=0; i<chars.length; i++)
-                updateBoard('B', chars[i].trim(), i);
+                B.put(chars[i].trim(), i);
+                //updateBoard('B', chars[i].trim(), i);
         }
+
+        updateBoard();
     }
 }
